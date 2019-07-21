c8ac3cbab353430bcece41cd967c1c8d001c2546
Blake <2979337714@qq.com>
2019-07-21 21:45:27 +0800

    伪造请求，由qq获取相关数据，基本完成search.js

--- src/index.js
+++ src/index.js
@@ -1,59 +1,77 @@
 import './main.css'
 import Slider from './js/slider'
+import Search from './js/search'
 import lazyload from './js/lazyload'
 import './js/tabs'
-import recommend from './json/recommend.json'
 import rank from './json/rank.json'
 
-let radioList = recommend.data.radioList
-let songList = recommend.data.songList
 let topList = rank.data.topList
+
+rankList(topList)
+
+fetch('http://localhost:4000/')
+  .then(res=>res.json())
+  .then(json=> {
+    render(json.data.slider)
+    radio(json.data.radioList)
+    song(json.data.songList)
+    lazyload(document.querySelectorAll('.lazyload'))
+  })
+
+new Search(document.querySelector('.search-view'))
+
+
 
 if (module.hot) {
   module.hot.accept();
 }
 
-render()
-
-document.querySelector('.radio-item').innerHTML = radioList.map((item)=>{
-  return `
-  <div class="item">
-    <a href="#">
-      <img class="pic lazyload" data-src="${item.picUrl}" alt="">
-    </a>
-    <div class="play"><img src="./imgs/list_sprite.png"></div>
-    <div class="title">${item.Ftitle}</div>
-  </div>
-  `
-}).join('')
-
-document.querySelector('.songList-item').innerHTML = songList.map((item)=>{
-  return `
-  <div class="item">
-    <a href="#">
-      <img class="pic lazyload" data-src="${item.picUrl}" alt="">
-    </a>
-    <div class="play"><img src="./imgs/list_sprite.png"></div>
-    <div class="title">${item.songListDesc}</div>
-  </div>
-  `
-}).join('')
-
-document.querySelector('.rank-item').innerHTML = topList.map((item)=>{
-  return `
-  <div class="item">
-    <div class="pic">
-      <img class="lazyload" data-src="${item.picUrl}" alt="">
+function radio(radioList){
+  document.querySelector('.radio-item').innerHTML = radioList.map((item)=>{
+    return `
+    <div class="item">
+      <a href="#">
+        <img class="pic lazyload" data-src="${item.picUrl}" alt="">
+      </a>
+      <div class="play"><img src="./imgs/list_sprite.png"></div>
+      <div class="title">${item.Ftitle}</div>
     </div>
-    <div class="content clearfix">
-      <div class="content-wrapper">
-        <h2>${item.topTitle}</h2>
-        ${songHandle(item)}
+    `
+  }).join('')
+}
+
+function song(songList){
+  document.querySelector('.songList-item').innerHTML = songList.map((item)=>{
+    return `
+    <div class="item">
+      <a href="#">
+        <img class="pic lazyload" data-src="${item.picUrl}" alt="">
+      </a>
+      <div class="play"><img src="./imgs/list_sprite.png"></div>
+      <div class="title">${item.songListDesc}</div>
+    </div>
+    `
+  }).join('')
+}
+
+function rankList(topList){
+  document.querySelector('.rank-item').innerHTML = topList.map((item)=>{
+    return `
+    <div class="item">
+      <div class="pic">
+        <img class="lazyload" data-src="${item.picUrl}" alt="">
+      </div>
+      <div class="content clearfix">
+        <div class="content-wrapper">
+          <h2>${item.topTitle}</h2>
+          ${songHandle(item)}
+        </div>
       </div>
     </div>
-  </div>
-  `
-}).join('')
+    `
+  }).join('')
+}
+
 
 function songHandle(item){
   return item.songList.map((song,index)=>
@@ -63,8 +81,8 @@ function songHandle(item){
   ).join('')
 }
 
-function render(){
-  let slides = recommend.data.slider.map(slide=>{
+function render(slider){
+  let slides = slider.map(slide=>{
     return {link: slide.linkUrl, image: slide.picUrl }
   })
   new Slider({
@@ -72,5 +90,3 @@ function render(){
     slides
   })
 }
-
-lazyload(document.querySelectorAll('.lazyload'))
