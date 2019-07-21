d9178e0a19c5834fb26eda1dd17c1fa7c82cefa0
Blake <2979337714@qq.com>
2019-07-21 09:53:16 +0800

    排行榜

--- src/index.js
+++ src/index.js
@@ -3,25 +3,17 @@ import Slider from './js/slider'
 import lazyload from './js/lazyload'
 import './js/tabs'
 import recommend from './json/recommend.json'
+import rank from './json/rank.json'
+
+let radioList = recommend.data.radioList
+let songList = recommend.data.songList
+let topList = rank.data.topList
 
 if (module.hot) {
   module.hot.accept();
 }
 
 render()
-
-function render(){
-  let slides = recommend.data.slider.map(slide=>{
-    return {link: slide.linkUrl, image: slide.picUrl }
-  })
-  new Slider({
-    el: document.querySelector('.item-wrapper'),
-    slides
-  })
-}
-
-let radioList = recommend.data.radioList
-let songList = recommend.data.songList
 
 document.querySelector('.radio-item').innerHTML = radioList.map((item)=>{
   return `
@@ -46,5 +38,39 @@ document.querySelector('.songList-item').innerHTML = songList.map((item)=>{
   </div>
   `
 }).join('')
+
+document.querySelector('.rank-item').innerHTML = topList.map((item)=>{
+  return `
+  <div class="item">
+    <div class="pic">
+      <img class="lazyload" data-src="${item.picUrl}" alt="">
+    </div>
+    <div class="content clearfix">
+      <div class="content-wrapper">
+        <h2>${item.topTitle}</h2>
+        ${songHandle(item)}
+      </div>
+    </div>
+  </div>
+  `
+}).join('')
+
+function songHandle(item){
+  return item.songList.map((song,index)=>
+    `
+    <p>${index+1}<span>${song.songname}</span>- ${song.singername}</p>
+    `
+  ).join('')
+}
+
+function render(){
+  let slides = recommend.data.slider.map(slide=>{
+    return {link: slide.linkUrl, image: slide.picUrl }
+  })
+  new Slider({
+    el: document.querySelector('.item-wrapper'),
+    slides
+  })
+}
 
 lazyload(document.querySelectorAll('.lazyload'))
