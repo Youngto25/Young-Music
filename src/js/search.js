0782a694f9710484394efdd329503f7ae3e2311c
Blake <2979337714@qq.com>
2019-07-22 16:20:29 +0800

    歌手栏 歌曲多页加载

--- src/js/search.js
+++ src/js/search.js
@@ -3,32 +3,76 @@ module.exports = class Search{
     this.$el = el
     this.$input = this.$el.querySelector('.input')
     this.$search = this.$el.querySelector('.icon-search')
+    this.$songs = []
     this.$keyword = ''
     this.$page = 1
+    this.$curpage = 1
+    this.$fetching = false
+    this.onscroll = this.onScroll.bind(this)
     this.getValue()
-    this.search()
+    window.addEventListener('scroll',this.onscroll)
+  }
+
+  onScroll(){
+    if(document.body.scrollHeight < document.documentElement.clientHeight + pageYOffset + 50){
+      this.search(this.$curpage + 1)
+    }
   }
 
   getValue(){
     this.$search.addEventListener('click',this.search.bind(this))
     this.$input.addEventListener('input',(event)=>{
-      this.$keyword = event.target.value
+      let keyword = event.target.value.trim()
+      if(!keyword) return this.reset()
+      this.$keyword = keyword
     })
   }
 
-  search(){
+  reset(){
+    this.$page = 1
+    this.$keyword = ''
+    this.$el.querySelector('.search-list').innerHTML = ''
+    this.$el.querySelector('.singer-list').innerHTML = ''
+  }
+
+  search(page){
+    if(this.$fetching)return
     if(!this.$keyword)return
-    fetch(`http://localhost:4000/search?keyword=${this.$keyword}&page=${this.$page}`)
+    this.$fetching = true
+    fetch(`http://localhost:4000/search?keyword=${this.$keyword}&page=${page || this.$page}`)
       .then(res=>res.json())
       .then(json=> {
-        console.log(json.data.song.list)
+        if(this.$curpage === 1){
+          this.createSinger(json.data.zhida)
+        }
         this.createList(json.data.song.list)
-    })
+        this.$songs.push(...json.data.song.list)
+        this.$curpage = json.data.song.curpage
+        console.log('songs')
+        console.log(this.$songs)
+      })
+      .then(()=>this.$fetching = false)
+      .catch(()=>this.$fetching = false)
+  }
+
+  createSinger(zhida){
+    if(!zhida.singername) return
+    this.$el.querySelector('.singer-list').innerHTML = `
+    <div class="list">
+      <div class="img">
+        <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000${zhida.singermid}.jpg?max_age=2592000" alt="">
+      </div>
+      <div class="detail">
+        <h2>${zhida.singername}</h2>
+        <p><span>单曲：${zhida.songnum}</span><span>专辑：${zhida.albumnum}</span></p>
+      </div>
+    </div>
+    `
   }
 
   createList(lists){
-    this.$el.querySelector('.search-list').innerHTML = lists.map(list=>{
-      return `
+    let html = lists.map(list=>
+      `
       <div class="list">
         <div class="img">
           <img src="./imgs/music.png" alt="">
@@ -39,7 +83,8 @@ module.exports = class Search{
         </div>
       </div>
       `
-    }).join('')
+    ).join('')
+    this.$el.querySelector('.search-list').insertAdjacentHTML('beforeend',html)
   }
 
   getSinger(singers){
